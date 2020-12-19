e685cac54fa6bd084e7a605a540e6da3b2e16770
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2020-12-19 14:50:47 -0300

    NewFace Blog

---
+++ assets/assets/markdown_posts/evolution/frustration.md
@@ -0,0 +1,77 @@
+# Frustration
+#### April 24, 2020
+#### EVOLUTION,LIFE,EN
+#### assets/frustrationimage.png
+
+### How the frustration brought me back to the game
+
+The year was 2019, I was entering the office of a big company for an interview, my second interview for an internship. We started the interview but, I was very nervous to explain, the best part of the interview was when they asked me to explain on the whiteboard about inheritance, polymorphism, and obviously I was unable to explain this. That was funny because it was not a hard exercise to explain.
+
+When left from the office, my brain started to break like and "kernel panic", that was the moment when I got very frustrated, the reason was clear. Before I left the office, I knew the answer and was very clear for me because of their body language. This day I felt very dumb.
+
+### Kernel Panic
+
+The worst and the best moment, in the next week, I stopped everything I was doing just to looking for the problem that was causing a "Kernel Panic". 
+
+### Restructuring my thinking
+
+My think is strange because everything needs to be structured, because of this I started again structuring my thinking and looking for mental vulnerabilities, and I found them, after finding them at the same time I started a "hotfix" to close them.
+
+### The Route
+
+After restructuring my thinking, I started to trace a route to achieve what I wanted. First I started to change the methodology, I was looking for ways to:
+
+- Understand the content fast.
+- Record fast the content.
+- Learn fast.
+
+I found the best way to learn and was with books, I decided to maintain a backlog to help me. You can found on my GitHub repository [BookStudy](https://github.com/raphaelbarbosaqwerty/BookStudy).
+
+### Learn fast with Bypasses
+
+#### Subliminal messages exploit
+
+I decided to exploit my brain at this time, I wanted to use the maximum capacity of my mind. From learning while sleeping and subliminal messages. The funniest way to learn is with Subliminal messages.
+
+I started my studies looking for how we can "inject" data on the brain just with the ambiance around (Subliminal). For weeks I had tested some methods using sheets, writing, whiteboard, I have noted this:
+
+- Whiteboard behind the monitor but your eye needs to see these words.
+- Write everything you want to learn on a sheet/notebook.
+- Post-it on the monitor with some words, jobs.
+- Sheets on your room / Mental maps around.
+- **Always write**.
+
+With patience and time, we can "inject" data on the brain. With this, we jump for the next exploit **"Sleeping exploit"**.
+
+#### Sleeping exploit
+
+This is more complex, best to do it on the weekends, vacation, and/or by night. When we read for so many hours we get sleepy and here we can start the exploit.
+
+After you read, make some annotations and you need to practice you'll get sleepy, go sleep. And that is the point our subconscious can absorb everything but if he finds something useless he ends up discarding it, because of this you need to make some annotations when you read or study.
+
+This is most simple, I have other "Sleeping exploit" more complex than this.
+
+### The rule
+
+Carry this into your life.  
+
+Just: **SHU HA RI**. 
+
+**SHU** - Follow the rule.   
+**HA** -  Be the rule.  
+**RI** -  break it.  
+  
+### The patience
+
+We need so much patience to this works fine. The results can be slow but work fine. Sometimes the great key is to teach the brain to relearn.    
+I was able to see the results some months after when I started a new project and wanted to learn more things some examples:
+
+- Laravel from scratch.
+- SOLID/CleanCode.
+- Swift from scratch.
+- Docker.
+- Agile.
+
+If it was not my frustration not being accepted in the begin I would never have found new ways to learn and this is how the frustration brought me back to the game. Special thanks for a close friend, who helped me with a push.
+
+**Remember you need to be always a beginner. You need to assume you don't know anything and always be willing to learn.**
---
+++ flutter_service_worker.js
@@ -0,0 +1,176 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"manifest.json": "dcaf1f9686117d3151039ad84fd9a229",
+"main.dart.js": "10ad1fb36b1dab6808dec5fcf49a69e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"assets/AssetManifest.json": "7b19b568b5792157570f206be3018abd",
+"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "a126c025bab9a1b4d8ac5534af76a208",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d80ca32233940ebadc5ae5372ccd67f9",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "831eb40a2d76095849ba4aecd4340f19",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/assets/cosmos.jpg": "96ec087955be1e4b9934d30b531bd2ec",
+"assets/assets/celtic.png": "b2187b05862c730db26f504648d075c5",
+"assets/assets/capture-ctf.jpg": "49158ac6f3afd9e776567100dd7a9325",
+"assets/assets/image_posts/ctf/rick/rickme.jpg": "4ba6cf450516ec08df13e01df3a24c75",
+"assets/assets/eye.png": "f77e3da163dd2bab680f03449d9eebc3",
+"assets/assets/idealamp.jpg": "dcaf3ad863dabe2fd579cc8512786da4",
+"assets/assets/markdown_posts/about/about-me.md": "8e4f86cdb507d8626aab17c4aa747cf7",
+"assets/assets/markdown_posts/about/the-blog.md": "90fa0489982839f4e146c94e13790890",
+"assets/assets/markdown_posts/informative/next-machines.md": "c13cc7ccfbc1a917768d22c524f29b7c",
+"assets/assets/markdown_posts/evolution/frustration.md": "cfc5b10563c803187f65f3cec444d0f6",
+"assets/assets/markdown_posts/ctf/rickdiculouslyeasy.md": "97b1ad45d0627ea884ec030bd4ecb6f3",
+"assets/assets/eye.jpg": "f77e3da163dd2bab680f03449d9eebc3",
+"assets/assets/frustrationimage.png": "9b71a4834be61e890befdf1c809662a2",
+"assets/NOTICES": "a1b687677849f1240cf0dc666bfbe084",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
+"index.html": "63c3c2f59070590766cac6cbcdcf4fa2",
+"/": "63c3c2f59070590766cac6cbcdcf4fa2"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "/",
+"main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#')) {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list, skip the cache.
+  if (!RESOURCES[key]) {
+    return event.respondWith(fetch(event.request));
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache. Ensure the resources are not cached
+        // by the browser for longer than the service worker expects.
+        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
+        return response || fetch(modifiedRequest).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    return self.skipWaiting();
+  }
+
+  if (event.message === 'downloadOffline') {
+    downloadOffline();
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey in Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
